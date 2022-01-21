b758f770927e11b3098ab61a62368c1ca6fbac33
Rodrigo Tognin <rotog@outlook.com>
2022-01-21 16:26:48 -0300

    Ajustes no css

--- resources/views/tasks/new.blade.php
+++ resources/views/tasks/new.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="/css/w3.css" type="text/css" rel="stylesheet">
+    <link href="{{ asset('css/w3.css') }}" rel="stylesheet">
     <title>Cadastrar uma nova tarefa</title>
 </head>
 <body>
--- resources/views/tasks/success.blade.php
+++ resources/views/tasks/success.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="/css/w3.css" type="text/css" rel="stylesheet">
+    <link href="{{ asset('css/w3.css') }}" rel="stylesheet">
     <title>Confirmação</title>
 </head>
 <body>
@@ -13,7 +13,7 @@
     <br><br>
     <p><b>{{ $message }}</b></p>
     <br><br>
-    <a href="/tasks">Início</a>
+    <a href="{{ route('tasks_home') }}">Início</a>
     
 </body>
 </html>
